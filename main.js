7efc0d6e4faca633a74227e5933fc06560aaf717
alejandropmz <alejandropacheco2497@gmail.com>
2023-03-10 20:22:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+/* arrays de zapatos */
+
+const menShoes = [
+  {
+    id: 1,
+    img: "images/men/zapatos-hombres-1.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 2,
+    img: "images/men/zapatos-hombres-2.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 3,
+    img: "images/men/zapatos-hombres-3.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 4,
+    img: "images/men/zapatos-hombres-4.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }
+
+]
+
+
+const womenShoes = [
+  {
+    id: 1,
+    img: "images/women/zapatos-mujer-1.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 2,
+    img: "images/women/zapatos-mujer-2.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 3,
+    img: "images/women/zapatos-mujer-3.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    id: 4,
+    img: "images/women/zapatos-mujer-4.jpg",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  }
+]
